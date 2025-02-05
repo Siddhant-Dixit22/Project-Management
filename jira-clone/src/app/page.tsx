82f9670dda69d2861c9dde885a3983ac47294d84
Siddhant-Dixit22 <dixit.siddhant22@gmail.com>
2025-02-05 16:58:02 -0500

    Setup Commit
    Set up NodeJs, added Shadnc, Testing to check if it work.

---
+++ jira-clone/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { TestComponent } from "@/features/test";
+
+export default function Home() {
+  return (
+    <div className="text-red-500">
+      <TestComponent></TestComponent>
+    </div>
+  );
+}
