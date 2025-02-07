0d8579acdfd8100675cf3a8bdac8534fb7836219
Siddhant-Dixit22 <dixit.siddhant22@gmail.com>
2025-02-07 01:48:02 -0500

    Authentication Page created
    Implemented `SignInCard` and `SignUpCard` component with email/password fields, validation, and social authentication buttons for Google and GitHub.  Created `SignInPage` & `SignUpPage` to render the card component. Added navigation to the sign-up page for users without an account.  Styled components using Tailwind for a responsive and modern UI. Also added layout file implementing a certain layout for the two pages.

--- jira-clone/src/app/page.tsx
+++ jira-clone/src/app/page.tsx
@@ -1,9 +1,29 @@
-import { TestComponent } from "@/features/test";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-red-500">
-      <TestComponent></TestComponent>
+    <div className="">
+      <Button size="lg">
+        Primary
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>
+      <Button variant="teritary">
+        Teritary
+      </Button>
     </div>
   );
 }
