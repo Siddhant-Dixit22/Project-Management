75f1c49e125a68afbf23fee356acc6911ba993e4
Siddhant-Dixit22 <dixit.siddhant22@gmail.com>
2025-03-04 15:10:47 -0500

    Edited Kanban Part
    Added the different statuses as headers in kanban area. Not much change.

--- jira-clone/src/features/tasks/components/task-view-switcher.tsx
+++ jira-clone/src/features/tasks/components/task-view-switcher.tsx
@@ -17,6 +17,7 @@ import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-fillers";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { DataKanban } from "./data-kanban";
 
 export const TaskViewSwitcher = () => {
     const [{
@@ -95,7 +96,7 @@ export const TaskViewSwitcher = () => {
                             <DataTable columns={columns} data={tasks?.documents ?? []}/>
                         </TabsContent>
                         <TabsContent value="kanban" className="mt-0">
-                            {JSON.stringify(tasks)}
+                            <DataKanban data={tasks?.documents ?? []} />
                         </TabsContent>
                         <TabsContent value="calender" className="mt-0">
                             {JSON.stringify(tasks)}
