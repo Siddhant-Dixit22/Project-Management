123091f86a2c8e7bd598a057df1e91e7416b6356
Siddhant-Dixit22 <dixit.siddhant22@gmail.com>
2025-03-08 21:53:03 -0500

    Project Analytics
    Made a project analytics section to see multiple parts, such as completed tasks, incomplete tasks, etc.

--- jira-clone/src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ jira-clone/src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Analytics } from "@/components/analytics";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
 import { Button } from "@/components/ui/button";
 import { useGetProject } from "@/features/projects/api/use-get-project";
+import { useGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { useProjectId } from "@/features/projects/hooks/use-project-id";
 import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
@@ -14,13 +16,16 @@ import Link from "next/link";
 
 export const ProjectIdClient = () => {
     const projectId = useProjectId();
-    const { data, isLoading } = useGetProject({ projectId });
+    const { data: project, isLoading: isLoadingProject } = useGetProject({ projectId });
+    const { data: analytics, isLoading: isLoadingAnalytics } = useGetProjectAnalytics({ projectId });  
+
+    const isLoading = isLoadingProject || isLoadingAnalytics;
 
     if (isLoading) {
         return <PageLoader />
     }
 
-    if (!data) {
+    if (!project) {
         return <PageError message="Project not found" />
     }
 
@@ -29,14 +34,14 @@ export const ProjectIdClient = () => {
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-x-2">
                     <ProjectAvatar 
-                        name={data.name}
-                        image={data.imageUrl}
+                        name={project.name}
+                        image={project.imageUrl}
                         className="size-8"
                     />
-                    <p className="text-lg font-semibold">{data.name}</p>
+                    <p className="text-lg font-semibold">{project.name}</p>
                 </div>
                 <div>
-                    <Link href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}>
+                    <Link href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}>
                         <Button variant="secondary" size="sm">
                             <PencilIcon className="size-4 mr-2"/>
                             Edit Project
@@ -44,6 +49,9 @@ export const ProjectIdClient = () => {
                     </Link>
                 </div>
             </div>
+            {analytics ? (
+                <Analytics {...analytics} />
+            ): null}
             <TaskViewSwitcher hideProjectFilter />
         </div>
     );
